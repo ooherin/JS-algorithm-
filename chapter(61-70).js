36b42d675db019a4fbfc74ae3b12646d49b2c6c4
ooherin <555ohr@naver.com>
2023-05-01 23:04:00 +0900

    7회차

---
+++ chapter(61-70).js
@@ -0,0 +1,110 @@
+//61 문자열 압축하기
+//aaabbbcdddd => a3b4c1d4
+let str = "aaabbbbcdddd";
+let answer = "";
+
+let arr = [...str];
+let obj = {};
+//객체에다가 1씩 증가시킨다
+for (let i = 0; i < arr.length; i++) {
+  obj[arr[i]] = obj[arr[i]] + 1 || 1;
+}
+//키와 value를 순회하며 answer에 더해준다
+for (let key in obj) {
+  answer += key;
+  answer += obj[key];
+}
+console.log(answer);
+//a3b4c1d4
+
+//62(pass)
+
+//63 줄임말 만들기
+//띄어쓰기 앞자리만 뽑아서 줄임말 만들기
+let words = "복잡한 세상 편하게 살자";
+let words2 = "마음의 상처";
+let splited = words2.split(" ");
+let answer1 = "";
+for (let word of splited) {
+  answer1 += word[0];
+}
+console.log(answer1); //복세편살 //마상
+
+//답안 (splice 이용)
+let splited2 = words2.split(" ");
+let result = "";
+for (let word of splited2) {
+  result += word.slice(0, 1);
+}
+
+//64 이상한 엘리베이터
+//7,3으로 정량을 최소단위로 맞추기
+//정량이 안되면 -1출력
+
+//7로 나누어서 나머지가 0이면 바로 리턴
+//나머지가 3,6이면 리턴
+//그외의 나머지이면 7을 하나씩 더해서 3으로 나눠보기
+//자기자신보다 크면 안됨
+function elevator(weight) {
+  let seven나머지 = weight % 7;
+  let seven몫 = Math.floor(weight / 7);
+  if (seven나머지 === 0) {
+    return seven몫;
+  }
+  if (seven나머지 % 3 === 0) {
+    return seven몫 + seven나머지 / 3;
+  }
+  while (seven몫 >= 0) {
+    seven몫 -= 1;
+    seven나머지 += 7;
+    if (seven나머지 % 3 === 0) {
+      return seven나머지 / 3;
+    }
+  }
+  return -1;
+}
+
+console.log(elevator(7)); //1
+console.log(elevator(17)); //3
+console.log(elevator(20)); //4
+
+//답안
+let result2 = 0;
+while (true) {
+  //7로 나누어떨어지면 몫 반환하고 break
+  if (N % 7 === 0) {
+    result += parseInt(N / 7, 10);
+    console.log(result);
+    break;
+  }
+  //3을 빼고 result에 1을 한번씩 늘린다.
+  //0이 될때까지 반복한다.
+  N -= 3;
+  result += 1;
+  //0이하이면 -1를 반환한다.
+  if (N < 0) {
+    console.log(-1);
+    break;
+  }
+}
+//65 변형된 리스트
+//a=[1,2,3,4] b=[a,b,c,d] 일떄 [[1,a],[2,b],[3,c],[4,d]]출력
+const a = [1, 2, 3, 4];
+const b = ["a", "b", "c", "d"];
+let answer2 = [];
+for (let i = 0; i < a.length; i++) {
+  if (i % 2 === 0) {
+    answer2.push([a[i], b[i]]);
+  } else {
+    answer2.push([b[i], a[i]]);
+  }
+}
+console.log(answer2);
+
+//답안(map 사용)
+a.map((e, index) => {
+  if (index % 2 === 0) {
+    return [a[i], b[i]];
+  }
+  return [b[i], a[i]];
+});
--- 프로그래머스/lv1/Q1-10.js
+++ 프로그래머스/lv1/Q1-10.js
@@ -1,4 +1,4 @@
-//Q1. 기사단원의 무기
+//Q1. 기사단원의 무기(완)
 //number의 각 약수를 배열로 구해서
 //약수가 limit보다 크면 power로 바꿔서
 //그 약수들의 합을 구하는 문제
@@ -45,7 +45,7 @@ function solution(number, limit, power) {
   return answer;
 }
 
-//Q2. 카드 뭉치
+//Q2. 카드 뭉치(완)
 //카드 뭉치 2개(card1, card2)에서 '앞에서부터' 차례대로 goal에 있는 단어들을 뽑아
 //끝까지 완성할 수 있으면 yes, 아니면 no를 반환
 function solution(cards1, cards2, goal) {
@@ -63,3 +63,84 @@ function solution(cards1, cards2, goal) {
   //모두 완료된다면 yes를 반환한다.
   return "Yes";
 }
+
+//Q3.수포자문제(미완)
+//패턴을 공식으로 만들어 푸는 방법(정답률70%)
+//왜 안되는지 모르겠음 => 다음에
+function solution(answers) {
+  //answers를 하나씩 순회하면서
+  //맞으면 +1, 안맞으면 +1을 하지 않는다
+  //수포자1은 index+1을 5로 나누었을 때 나머지
+  //수포자2는 index가 짝수일때는 2, 홀수일때는 [1,3,4,5]반복?
+  //수포자3은[3,3,1,1,2,2,4,4,5,5]
+  let answer1 = 0;
+  let answer2 = 0;
+  let answer3 = 0;
+  answers.forEach((e, index) => {
+    let arr3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
+    if ((index + 1) % 5 === e) {
+      answer1++;
+    } else {
+    }
+    if (index % 2 === 0 && e === 2) {
+      answer2++;
+    } else if (index % 2 === 1) {
+      let newIndex = ((index - 1) / 2) % 4;
+      let num = [1, 3, 4, 5][newIndex];
+      if (num === e) {
+        answer2++;
+      }
+    }
+    if (arr3[index % 10] === e) {
+      answer3++;
+    } else {
+    }
+  });
+  return val_return(answer1, answer2, answer3);
+}
+
+//반복패턴으로 푸는 방법(정답률100%)
+function solution(answers) {
+  let stu1 = [1, 2, 3, 4, 5];
+  let stu2 = [2, 1, 2, 3, 2, 4, 2, 5];
+  let stu3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
+  let answer1 = 0;
+  let answer2 = 0;
+  let answer3 = 0;
+  for (let i = 0; i < answers.length; i++) {
+    if (answers[i] === stu1[i % 5]) {
+      answer1++;
+    }
+    if (answers[i] === stu2[i % 8]) {
+      answer2++;
+    }
+    if (answer[i] === stu3[i % 10]) {
+      answer3++;
+    }
+  }
+  answers = val_return(answer1, answer2, answer3);
+  return answers;
+}
+//answer1,answer2, answer3의 key를 각각 1,2,3으로 지정한
+//객체를 만든다
+//value를 비교하여 가장 큰 key값을 배열로 반환한다.
+
+// function val_return(a, b, c) {
+//   let val = [];
+
+//   if (a > b && a > c) val = [1];
+//   if (a > b && a === c) val = [1, 3];
+//   if (a > b && a < c) val = [3];
+
+//   if (a === b && a > c) val = [1, 2];
+//   if (a === b && a === c) val = [1, 2, 3];
+//   if (a === b && a < c) val = [3];
+
+//   if (a < b && b < c) val = [3];
+//   if (a < b && b === c) val = [2, 3];
+//   if (a < b && b > c) val = [2];
+
+//   if (a === 0 && b === 0 && c === 0) val = [];
+
+//   return val;
+// }
