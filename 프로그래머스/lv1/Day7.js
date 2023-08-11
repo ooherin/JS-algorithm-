3f9fc2a77d159b357767311ca5ad14f037443750
ooherin <555ohr@naver.com>
2023-08-12 00:48:16 +0900

    feat: day7 추가

--- 프로그래머스/lv1/Day7.js
+++ 프로그래머스/lv1/Day7.js
@@ -38,4 +38,87 @@ function solution(participant, completion) {
   return map;
 }
 
-console.log(solution(["지형", "혜린"], ["지형"]));
+//폰켓몬
+function solution(nums) {
+  const obj = nums.reduce((acc, cur) => {
+    acc[String(cur)] = (acc[String(cur)] || 0) + 1;
+    return acc;
+  }, {});
+  const length = Object.keys(obj);
+  //const arr = [...new Set(nums)];
+
+  if (nums.length / 2 >= length.length) {
+    return length.length;
+  }
+  return nums.length / 2;
+}
+
+//다른 풀이
+function solution(nums) {
+  const arr = [...new Set(nums)]; //고유한 숫자 개수 구하기
+  return Math.min(nums.length / 2, arr.length);
+  //고유한 숫자 개수보다 폰켓몬 개수가 작으면, 폰켓몬 개수
+  //고유한 숫자가 더 작으면, 고유한 숫자 개수
+}
+
+//체육복 빌려주기
+
+function solution(n, lost, reserve) {
+  let answer = n;
+  let Obj = {};
+  for (let i = 1; i <= n; i++) {
+    Obj[i.toString()] = 1;
+  }
+  reserve.forEach((e) => {
+    if (Obj[e.toString()] > 0) {
+      Obj[e.toString()] += 1;
+    }
+  });
+
+  lost.forEach((e, idx) => {
+    let prev = idx > 0 ? idx - 1 : false;
+    let next = idx < n ? idx + 1 : false;
+
+    if (Obj[e.toString()] > 0) {
+      Obj[e.toString()] -= 1;
+    } else if (Obj[prev.toString()] > 0) {
+      Obj[prev.toString()] -= 1;
+    } else if (Obj[next.toString()] > 0) {
+      Obj[next.toString()] -= 1;
+    } else {
+      answer -= 1;
+    }
+  });
+  return answer;
+}
+//=> 배열로도 충분히 풀 수 있따.!
+//정답 풀이
+
+function solution(n, lost, reserve) {
+  let answer = n;
+  let students = Array.from({ length: n }, () => 1); // Initialize all students with 1 (have a uniform)
+
+  // Distribute extra uniforms
+  reserve.forEach((student) => {
+    students[student - 1]++;
+  });
+
+  // Process lost students
+  lost.forEach((student) => {
+    if (students[student - 1] > 1) {
+      // If student has an extra uniform, use it
+      students[student - 1]--;
+    } else if (student > 1 && students[student - 2] > 1) {
+      // Borrow from previous student if available
+      students[student - 2]--;
+    } else if (student < n && students[student] > 1) {
+      // Borrow from next student if available
+      students[student]--;
+    } else {
+      // No available uniforms, reduce the answer
+      answer--;
+    }
+  });
+
+  return answer;
+}
